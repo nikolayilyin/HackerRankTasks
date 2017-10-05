17440d4d942e494c9ae70e46a9ef43fc56df2239
irishwithaxe <irishwithaxe@gmail.com>
2017-10-05 13:30:03 +0300

    init new task
    init Roads adn Libraries task

---
+++ Tasks/Algorithms/GraphTheory/RoadsAndLibraries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tasks.Algorithms.GraphTheory
+{
+    //https://www.hackerrank.com/challenges/torque-and-development/problem
+    public class RoadsAndLibraries
+    {
+        public RoadsAndLibraries()
+        {
+        }
+
+        public long NumberOfCities { get; set; }
+
+        public long NumberOfRoads { get; set; }
+
+        public long CostToBuildLibrary { get; set; }
+
+        public long CostToRepairRoad { get; set; }
+
+        public void AddCityRoad(long c1, long c2)
+        {
+        }
+
+        /// <summary>
+        /// minimum cost of making libraries accessible to all the citizens on a new line.
+        /// </summary>
+        public long MinimumCost { get { return 0L; } }
+    }
+}
---
+++ XUnitTestProject/Algorithms/GraphTheory/RoadsAndlibrariesTest1.cs
@@ -0,0 +1,47 @@
+﻿using Tasks.Algorithms.GraphTheory;
+using Xunit;
+
+namespace XUnitTestProject.Algorithms.GraphTheory
+{
+    public class RoadsAndlibrariesTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var ral = new RoadsAndLibraries
+            {
+                NumberOfCities = 3,
+                NumberOfRoads = 3,
+                CostToBuildLibrary = 2,
+                CostToRepairRoad = 1
+            };
+
+            ral.AddCityRoad(1, 2);
+            ral.AddCityRoad(3, 1);
+            ral.AddCityRoad(2, 3);
+
+            Assert.Equal(4, ral.MinimumCost);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var ral = new RoadsAndLibraries
+            {
+                NumberOfCities = 6,
+                NumberOfRoads = 6,
+                CostToBuildLibrary = 2,
+                CostToRepairRoad = 5
+            };
+
+            ral.AddCityRoad(1, 3);
+            ral.AddCityRoad(3, 4);
+            ral.AddCityRoad(2, 4);
+            ral.AddCityRoad(1, 2);
+            ral.AddCityRoad(2, 3);
+            ral.AddCityRoad(5, 6);
+
+            Assert.Equal(12, ral.MinimumCost);
+        }
+    }
+}
