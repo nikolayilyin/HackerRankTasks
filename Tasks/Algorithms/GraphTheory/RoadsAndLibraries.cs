574c9aff580e64a2c10f986906d82a4bff3f0604
irishwithaxe <irishwithaxe@gmail.com>
2017-10-05 18:12:49 +0300

    first try
    too slow

--- Tasks/Algorithms/GraphTheory/RoadsAndLibraries.cs
+++ Tasks/Algorithms/GraphTheory/RoadsAndLibraries.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Tasks.Algorithms.GraphTheory
@@ -7,6 +8,8 @@ namespace Tasks.Algorithms.GraphTheory
     //https://www.hackerrank.com/challenges/torque-and-development/problem
     public class RoadsAndLibraries
     {
+        public List<HashSet<long>> HSs = new List<HashSet<long>>();
+
         public RoadsAndLibraries()
         {
         }
@@ -19,13 +22,70 @@ namespace Tasks.Algorithms.GraphTheory
 
         public long CostToRepairRoad { get; set; }
 
-        public void AddCityRoad(long c1, long c2)
+        public void AddCityRoad(long city1, long city2)
         {
+            HashSet<long> a1hs = null;
+            HashSet<long> a2hs = null;
+
+            foreach (var hs in HSs)
+            {
+                var isa1 = hs.Contains(city1);
+                var isa2 = hs.Contains(city2);
+                if (isa1 && isa2)
+                    return;
+                if (isa1)
+                    a1hs = hs;
+                if (isa2)
+                    a2hs = hs;
+
+                if (a1hs != null && a2hs != null)
+                    break;
+            }
+
+            if (a1hs != null && a2hs != null)
+            {
+                a1hs.UnionWith(a2hs);
+                HSs.Remove(a2hs);
+                return;
+            }
+
+            if (a1hs != null)
+            {
+                a1hs.Add(city2);
+                return;
+            }
+
+            if (a2hs != null)
+            {
+                a2hs.Add(city1);
+                return;
+            }
+
+            HSs.Add(new HashSet<long> { city1, city2 });
         }
+
+        private long CalcMinCost()
+        {
+            if (CostToBuildLibrary <= CostToRepairRoad)
+                return NumberOfCities * CostToBuildLibrary;
+
+            var arr = HSs.Select(x => (long)x.Count).ToArray();
+            var hssum = arr.Sum();
+
+            var unconnectedCitys = NumberOfCities - hssum;
+
+            var money = unconnectedCitys * CostToBuildLibrary;
+
+            for (var i = 0; i < arr.Length; i++)
+                money += (arr[i] - 1) * CostToRepairRoad + CostToBuildLibrary;
+
+            return money;
+        }
+
 
         /// <summary>
         /// minimum cost of making libraries accessible to all the citizens on a new line.
         /// </summary>
-        public long MinimumCost { get { return 0L; } }
+        public long MinimumCost { get { return CalcMinCost(); } }
     }
 }
