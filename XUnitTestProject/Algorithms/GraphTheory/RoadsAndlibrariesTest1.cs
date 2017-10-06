e211353efae728a9b6905a6425ef668f3369446b
irishwithaxe <irishwithaxe@gmail.com>
2017-10-06 11:09:56 +0300

    first slow solution
    plus some test data
    first solution does not satisfy the execution time requirement

--- XUnitTestProject/Algorithms/GraphTheory/RoadsAndlibrariesTest1.cs
+++ XUnitTestProject/Algorithms/GraphTheory/RoadsAndlibrariesTest1.cs
@@ -3,8 +3,15 @@ using Xunit;
 
 namespace XUnitTestProject.Algorithms.GraphTheory
 {
-    public class RoadsAndlibrariesTest1
+    public class RoadsAndLibrariesTest1
     {
+        public RoadsAndLibrariesTest1()
+        {
+            //RoadsAndLibrariesTestData1RawInput.input5.SplitPairs().WritePairsToFile("test5");
+            //RoadsAndLibrariesTestData1RawInput.input8.SplitPairs().WritePairsToFile("test8");
+            //RoadsAndLibrariesTestData1RawInput.input3.SplitPairs().WritePairsToFile("test3");
+        }
+
         [Fact]
         public void Test1()
         {
@@ -43,5 +50,217 @@ namespace XUnitTestProject.Algorithms.GraphTheory
 
             Assert.Equal(12, ral.MinimumCost);
         }
+
+        [Fact]
+        public void Test30()
+        {
+            var inparr = RoadsAndLibrariesTestData1.inputparams0;
+            var pairs = RoadsAndLibrariesTestData1.inputpairs0;
+            var output = RoadsAndLibrariesTestData1.output0;
+
+            var ral = new RoadsAndLibraries
+            {
+                NumberOfCities = inparr[0],
+                NumberOfRoads = inparr[1],
+                CostToBuildLibrary = inparr[2],
+                CostToRepairRoad = inparr[3]
+            };
+
+            for (var i = 0; i < pairs.Length / 2; i++)
+                ral.AddCityRoad(pairs[i, 0], pairs[i, 1]);
+
+            Assert.Equal(output, ral.MinimumCost);
+        }
+
+        [Fact]
+        public void Test31()
+        {
+            var inparr = RoadsAndLibrariesTestData1.inputparams1;
+            var pairs = RoadsAndLibrariesTestData1.inputpairs1;
+            var output = RoadsAndLibrariesTestData1.output1;
+
+            var ral = new RoadsAndLibraries
+            {
+                NumberOfCities = inparr[0],
+                NumberOfRoads = inparr[1],
+                CostToBuildLibrary = inparr[2],
+                CostToRepairRoad = inparr[3]
+            };
+
+            for (var i = 0; i < pairs.Length / 2; i++)
+                ral.AddCityRoad(pairs[i, 0], pairs[i, 1]);
+
+            Assert.Equal(output, ral.MinimumCost);
+        }
+
+        [Fact]
+        public void Test32()
+        {
+            var inparr = RoadsAndLibrariesTestData1.inputparams2;
+            var pairs = RoadsAndLibrariesTestData1.inputpairs2;
+            var output = RoadsAndLibrariesTestData1.output2;
+
+            var ral = new RoadsAndLibraries
+            {
+                NumberOfCities = inparr[0],
+                NumberOfRoads = inparr[1],
+                CostToBuildLibrary = inparr[2],
+                CostToRepairRoad = inparr[3]
+            };
+
+            for (var i = 0; i < pairs.Length / 2; i++)
+                ral.AddCityRoad(pairs[i, 0], pairs[i, 1]);
+
+            Assert.Equal(output, ral.MinimumCost);
+        }
+
+        [Fact]
+        public void Test33()
+        {
+            var inparr = RoadsAndLibrariesTestData1.inputparams3;
+            var pairs = RoadsAndLibrariesTestData1.inputpairs3;
+            var output = RoadsAndLibrariesTestData1.output3;
+
+            var ral = new RoadsAndLibraries
+            {
+                NumberOfCities = inparr[0],
+                NumberOfRoads = inparr[1],
+                CostToBuildLibrary = inparr[2],
+                CostToRepairRoad = inparr[3]
+            };
+
+            for (var i = 0; i < pairs.Length / 2; i++)
+                ral.AddCityRoad(pairs[i, 0], pairs[i, 1]);
+
+            Assert.Equal(output, ral.MinimumCost);
+        }
+
+        [Fact]
+        public void Test34()
+        {
+            var inparr = RoadsAndLibrariesTestData1.inputparams4;
+            var pairs = RoadsAndLibrariesTestData1.inputpairs4;
+            var output = RoadsAndLibrariesTestData1.output4;
+
+            var ral = new RoadsAndLibraries
+            {
+                NumberOfCities = inparr[0],
+                NumberOfRoads = inparr[1],
+                CostToBuildLibrary = inparr[2],
+                CostToRepairRoad = inparr[3]
+            };
+
+            for (var i = 0; i < pairs.Length / 2; i++)
+                ral.AddCityRoad(pairs[i, 0], pairs[i, 1]);
+
+            Assert.Equal(output, ral.MinimumCost);
+        }
+
+        [Fact]
+        public void Test35()
+        {
+            var inparr = RoadsAndLibrariesTestData1.inputparams5;
+            var pairs = RoadsAndLibrariesTestData1.inputpairs5;
+            var output = RoadsAndLibrariesTestData1.output5;
+
+            var ral = new RoadsAndLibraries
+            {
+                NumberOfCities = inparr[0],
+                NumberOfRoads = inparr[1],
+                CostToBuildLibrary = inparr[2],
+                CostToRepairRoad = inparr[3]
+            };
+
+            for (var i = 0; i < pairs.Length / 2; i++)
+                ral.AddCityRoad(pairs[i, 0], pairs[i, 1]);
+
+            Assert.Equal(output, ral.MinimumCost);
+        }
+
+        [Fact]
+        public void Test36()
+        {
+            var inparr = RoadsAndLibrariesTestData1.inputparams6;
+            var pairs = RoadsAndLibrariesTestData1.inputpairs6;
+            var output = RoadsAndLibrariesTestData1.output6;
+
+            var ral = new RoadsAndLibraries
+            {
+                NumberOfCities = inparr[0],
+                NumberOfRoads = inparr[1],
+                CostToBuildLibrary = inparr[2],
+                CostToRepairRoad = inparr[3]
+            };
+
+            for (var i = 0; i < pairs.Length / 2; i++)
+                ral.AddCityRoad(pairs[i, 0], pairs[i, 1]);
+
+            Assert.Equal(output, ral.MinimumCost);
+        }
+
+        [Fact]
+        public void Test37()
+        {
+            var inparr = RoadsAndLibrariesTestData1.inputparams7;
+            var pairs = RoadsAndLibrariesTestData1.inputpairs7;
+            var output = RoadsAndLibrariesTestData1.output7;
+
+            var ral = new RoadsAndLibraries
+            {
+                NumberOfCities = inparr[0],
+                NumberOfRoads = inparr[1],
+                CostToBuildLibrary = inparr[2],
+                CostToRepairRoad = inparr[3]
+            };
+
+            for (var i = 0; i < pairs.Length / 2; i++)
+                ral.AddCityRoad(pairs[i, 0], pairs[i, 1]);
+
+            Assert.Equal(output, ral.MinimumCost);
+        }
+
+        [Fact]
+        public void Test38()
+        {
+            var inparr = RoadsAndLibrariesTestData1.inputparams8;
+            var pairs = RoadsAndLibrariesTestData1.inputpairs8;
+            var output = RoadsAndLibrariesTestData1.output8;
+
+            var ral = new RoadsAndLibraries
+            {
+                NumberOfCities = inparr[0],
+                NumberOfRoads = inparr[1],
+                CostToBuildLibrary = inparr[2],
+                CostToRepairRoad = inparr[3]
+            };
+
+            for (var i = 0; i < pairs.Length / 2; i++)
+                ral.AddCityRoad(pairs[i, 0], pairs[i, 1]);
+
+            Assert.Equal(output, ral.MinimumCost);
+        }
+
+        [Fact]
+        public void Test39()
+        {
+            var inparr = RoadsAndLibrariesTestData1.inputparams9;
+            var pairs = RoadsAndLibrariesTestData1.inputpairs9;
+            var output = RoadsAndLibrariesTestData1.output9;
+
+            var ral = new RoadsAndLibraries
+            {
+                NumberOfCities = inparr[0],
+                NumberOfRoads = inparr[1],
+                CostToBuildLibrary = inparr[2],
+                CostToRepairRoad = inparr[3]
+            };
+
+            for (var i = 0; i < pairs.Length / 2; i++)
+                ral.AddCityRoad(pairs[i, 0], pairs[i, 1]);
+
+            Assert.Equal(output, ral.MinimumCost);
+        }
+
+
     }
 }
---
+++ XUnitTestProject/Extensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace XUnitTestProject
+{
+    public static class Extensions
+    {
+        public static string[] SplitPairs(this string str)
+        {
+            return str.Split(new[] { " ", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public static void WritePairsToFile(this string[] pairs, [CallerMemberName] string methodName = "")
+        {
+            var filename = methodName + ".txt";
+
+            if (File.Exists(filename))
+                File.Delete(filename);
+
+            using (var file = File.OpenWrite(filename))
+            using (var writer = new StreamWriter(file))
+            {
+                writer.WriteLine("\t\tpublic static int[,] inputp" + methodName + " = {");
+
+                var cntr = 0;
+
+                for (var i = 0; i < pairs.Length; i += 2)
+                {
+                    if (cntr == 0)
+                        writer.Write("\t\t\t");
+                    writer.Write("{" + pairs[i] + ", " + pairs[i + 1] + "}");
+                    if (cntr++ < 8)
+                        writer.Write(", ");
+                    else
+                    {
+                        cntr = 0;
+                        writer.WriteLine(",");
+                    }
+                }
+
+                writer.WriteLine(" };\r\n\r\n");
+            }
+        }
+    }
+}
