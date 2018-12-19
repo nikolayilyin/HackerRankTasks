84b203ad314cfbf82cd056b208cb2e56c5a31ac4
irishwithaxe <irishwithaxe@gmail.com>
2018-12-20 00:11:30 +0300

    birthday chocolate problem solved

---
+++ Tasks/BirthdayChocolate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Tasks {
+	// https://www.hackerrank.com/challenges/the-birthday-bar/problem
+	public class BirthdayChocolate {
+
+		// Complete the birthday function below.
+		public static int birthday(List<int> s, int d, int m) {
+			int ways = 0;
+
+			if (s.Count < m) {
+				return ways;
+			}
+
+			for (int i = 0; i < s.Count - m + 1; i++) {
+				int sum = 0;
+				for(int j = 0; j< m; j++) {
+					sum += s[i + j];
+				}
+
+				if(sum == d) {
+					ways++;
+				}
+			}
+
+			return ways;
+		}
+	}
+}
---
+++ XUnitTestProject1/BirthdayChocolateTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using Tasks;
+using Xunit;
+
+namespace XUnitTestProject1 {
+	public class BirthdayChocolateTest {
+		[Theory]
+		[InlineData(new[] { 4 }, 4, 1, 1)]
+		[InlineData(new[] { 1, 1, 1, 1, 1, 1 }, 3, 2, 0)]
+		[InlineData(new[] { 1, 2, 1, 3, 2 }, 3, 2, 2)]
+		public void Test(int[] bar, int day, int month, int expected) {
+			int result = BirthdayChocolate.birthday(bar.ToList(), day, month);
+			Assert.Equal(expected, result);
+		}
+	}
+}
